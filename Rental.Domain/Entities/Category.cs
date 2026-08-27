f83a5cff244ece9d7872410a9f08f66cfd3a0b4d
Breno Rodrigues Dos Santos <brenobr10@hotmail.com>
2026-08-27 19:22:48 -0300

    ajuste entidade category

--- Rental.Domain/Entities/Category.cs
+++ Rental.Domain/Entities/Category.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Rental.Domain.Validation;
+using System;
 
 namespace Rental.Domain.Entities
 {
@@ -6,40 +7,38 @@ namespace Rental.Domain.Entities
     {
         public int Id { get; private set; }
         public string Name { get; private set; }
- 
+
+        public Category(string name)
+        {
+            ValidateName(name);
+        }
+
         public Category(int id, string name)
         {
-            if(id <= 0)
-            {
-                throw new ArgumentException("O Id da categoria deve ser maior que zero.");
-            }
+            DomainExceptionValidation.When(id < 0, "O Id da categoria não pode ser negativo.");
             Id = id;
-            Name = name.Trim();
             ValidateName(name);
         }
 
         public void Update(string name)
         {
-            Name = name.Trim();
             ValidateName(name);
         }
 
         private void ValidateName(string name)
         {
-            if (string.IsNullOrWhiteSpace(name))
-            {
-                throw new ArgumentException("O nome da categoria não pode ser vazio.");
-            }
+           DomainExceptionValidation.When(string.IsNullOrEmpty(name), 
+               "O nome da categoria não pode ser nulo ou vazio.");
 
-            if (name.Length > 100)
-            {
-                throw new ArgumentException("O nome da categoria não pode ter mais de 100 caracteres.");
-            }
+            string trimmedName = name.Trim();
 
-            if (name.Length < 3)
-            {
-                throw new ArgumentException("O nome da categoria deve ter pelo menos 3 caracteres.");
-            }
+            DomainExceptionValidation.When(trimmedName.Length < 3, 
+                "O nome da categoria deve ter no mínimo 3 caracteres.");
+
+            DomainExceptionValidation.When(trimmedName.Length > 50,
+                "O nome da categoria deve ter no máximo 50 caracteres.");
+
+            Name = trimmedName;
         }
     }
 }
