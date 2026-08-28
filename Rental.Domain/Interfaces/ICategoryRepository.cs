7e6df7f202e27c5d97e4553a3fb1d19794f9a0f9
Breno Rodrigues Dos Santos <brenobr10@hotmail.com>
2026-08-28 20:17:24 -0300

    cria contrato dos repositorios para produtos e categorias

---
+++ Rental.Domain/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using Rental.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rental.Domain.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        Task<IEnumerable<Category>> GetAllCategoriesAsync();
+        Task<IEnumerable<Category>> GetCategoriesByNameAsync(string name);
+        Task<Category> GetCategoryByIdAsync(Guid id);
+        Task AddCategoryAsync(Category category);
+        Task UpdateCategoryAsync(Category category);
+        Task DeleteCategoryAsync(Guid id);
+    }
+}
---
+++ Rental.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using Rental.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rental.Domain.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetAllProductsAsync();
+        Task<IEnumerable<Product>> GetProductsByCategoryIdAsync(Guid categoryId);
+        Task<IEnumerable<Product>> GetProductsByNameAsync(string name);
+        Task<Product> GetProductByIdAsync(Guid id);
+        Task AddProductAsync(Product product);
+        Task UpdateProductAsync(Product product);
+        Task DeleteProductAsync(Guid id);
+
+    }
+}
