dab4de6f37ef9247aadd193f44ba5e7cfe1462c7
Breno Rodrigues Dos Santos <brenobr10@hotmail.com>
2026-09-01 20:15:38 -0300

    configura as entidades com Fluent API

---
+++ Rental.Infra.Data/EntitiesConfiguration/ProductConfiguration.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Rental.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rental.Infra.Data.EntitiesConfiguration
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+           builder.HasKey(p => p.Id);
+
+              builder.Property(p => p.Name)
+                 .IsRequired()
+                 .HasMaxLength(100);
+
+             builder.Property(p => p.Description)
+                  .IsRequired()
+                 .HasMaxLength(500);
+
+             builder.Property(p => p.Price)
+                 .IsRequired()
+                 .HasColumnType("decimal(10,2)");
+
+            builder.Property(p => p.Quantity)
+                   .IsRequired();
+
+            builder.Property(p => p.ImageUrl)
+                   .HasMaxLength(500)
+                   .IsRequired();
+
+            builder.Property(p => p.Available)
+                   .IsRequired()
+                   .HasDefaultValue(true);
+
+            builder.Property(p => p.CategoryId)
+                 .IsRequired();     
+
+             builder.HasOne(p => p.Category)         
+                 .WithMany(c => c.Products)
+                 .HasForeignKey(p => p.CategoryId)
+                 .IsRequired();
+        }
+    }
+}
