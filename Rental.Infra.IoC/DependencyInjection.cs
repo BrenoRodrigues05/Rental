a5e0acd51454ac6fa84c3fa2b87a0ca468878b37
Breno Rodrigues Dos Santos <brenobr10@hotmail.com>
2026-09-06 13:11:41 -0300

    cria a classe de injecao de dependencia

---
+++ Rental.Infra.IoC/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Rental.Domain.Interfaces;
+using Rental.Infra.Data.Context;
+using Rental.Infra.Data.Repositories;
+
+namespace Rental.Infra.IoC
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"),
+                    b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+
+            return services;
+        }
+    }
+}
