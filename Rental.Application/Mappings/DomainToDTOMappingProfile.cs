4cc05fa32a00a238a2c342e8ab5f54c652be1108
Breno Rodrigues Dos Santos <brenobr10@hotmail.com>
2026-09-06 14:06:29 -0300

    cria mapeamento com AutoMapper

---
+++ Rental.Application/Mappings/DomainToDTOMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Rental.Application.DTOs;
+using Rental.Domain.Entities;
+
+namespace Rental.Application.Mappings
+{
+    public class DomainToDTOMappingProfile : Profile
+    {
+        public DomainToDTOMappingProfile()
+        {
+            CreateMap<Category, CategoryDTO>().ReverseMap();
+            CreateMap<Product, ProductDTO>().ReverseMap();
+        }
+    }
+}
