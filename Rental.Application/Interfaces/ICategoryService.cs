c2f10e3a921ecbc8f60af18781ba7dfb1f63af75
Breno Rodrigues Dos Santos <brenobr10@hotmail.com>
2026-09-15 19:16:58 -0300

    cria contratos de servico para as entidades

---
+++ Rental.Application/Interfaces/ICategoryService.cs
@@ -0,0 +1,18 @@
+﻿using Rental.Application.DTOs;
+
+namespace Rental.Application.Interfaces
+{
+    public interface ICategoryService
+    {
+        Task<IEnumerable<CategoryDTO>> GetAllCategoriesAsync();
+        Task<CategoryDTO> GetCategoryByIdAsync(Guid id);
+
+        Task<IEnumerable<CategoryDTO>> GetCategoriesByNameAsync(string name);
+
+        Task<CategoryDTO> CreateCategoryAsync(CategoryDTO categoryDto);
+
+        Task<CategoryDTO> UpdateCategoryAsync(CategoryDTO categoryDto);
+
+        Task<bool> DeleteCategoryAsync(Guid id);
+    }
+}
---
+++ Rental.Application/Interfaces/IProductService.cs
@@ -0,0 +1,15 @@
+﻿using Rental.Application.DTOs;
+
+namespace Rental.Application.Interfaces
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductDTO>> GetAllProductsAsync();
+        Task<ProductDTO> GetProductByIdAsync(Guid id);
+        Task<IEnumerable<ProductDTO>> GetProductsByCategoryAsync(Guid categoryId);
+        Task<IEnumerable<ProductDTO>> GetProductsByNameAsync(string name);
+        Task<ProductDTO> CreateProductAsync(ProductDTO productDto);
+        Task<ProductDTO> UpdateProductAsync(ProductDTO productDto);
+        Task<bool> DeleteProductAsync(Guid id);
+    }
+}
