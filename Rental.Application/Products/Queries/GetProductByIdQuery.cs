5647c5e35182fa9cbd64720423a724d20c432fb5
Breno Rodrigues Dos Santos <brenobr10@hotmail.com>
2026-09-16 19:31:05 -0300

    CQRS - implementa queries para Product

---
+++ Rental.Application/Products/Queries/GetProductByIdQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using Rental.Domain.Entities;
+
+namespace Rental.Application.Products.Queries
+{
+    public class GetProductByIdQuery : IRequest<Product>
+    {
+        public Guid Id { get; set; }
+        public GetProductByIdQuery(Guid id)
+        {
+            Id = id;
+        }
+    }
+}
---
+++ Rental.Application/Products/Queries/GetProductsQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using Rental.Domain.Entities;
+
+namespace Rental.Application.Products.Queries
+{
+    public class GetProductsQuery : IRequest<IEnumerable<Product>>
+    {
+    }
+}
