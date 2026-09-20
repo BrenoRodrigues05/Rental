37aa0344a76e14129b3335552b31ee087b3316ae
Breno Rodrigues Dos Santos <brenobr10@hotmail.com>
2026-09-20 12:56:37 -0300

    CQRS - cria query e handler para busca de products pelo ID da category

---
+++ Rental.Application/Products/Handlers/GetProductsByCategoryIdQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Rental.Application.Products.Queries;
+using Rental.Domain.Entities;
+using Rental.Domain.Interfaces;
+
+namespace Rental.Application.Products.Handlers
+{
+    public class GetProductsByCategoryIdQueryHandler : IRequestHandler<GetProductsByCategoryIdQuery, 
+        IEnumerable<Product>>
+    {
+        private readonly IProductRepository _productRepository;
+
+        public GetProductsByCategoryIdQueryHandler(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public async Task<IEnumerable<Product>> Handle(GetProductsByCategoryIdQuery request,
+            CancellationToken cancellationToken)
+        {
+           var products = await _productRepository.GetProductsByCategoryIdAsync(request.CategoryId);
+    
+            return products;
+        }
+    }
+}
---
+++ Rental.Application/Products/Queries/GetProductsByCategoryIdQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Rental.Domain.Entities;
+
+namespace Rental.Application.Products.Queries
+{
+    public class GetProductsByCategoryIdQuery : IRequest<IEnumerable<Product>>
+    {
+        public Guid CategoryId { get; }
+
+        public GetProductsByCategoryIdQuery(Guid categoryId)
+        {
+            CategoryId = categoryId;
+        }
+    }
+}
