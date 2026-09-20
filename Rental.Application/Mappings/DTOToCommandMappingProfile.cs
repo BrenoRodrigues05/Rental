0d1aba94d42f67a19127c57e857b75e4c82cdb21
Breno Rodrigues Dos Santos <brenobr10@hotmail.com>
2026-09-20 14:30:24 -0300

    CQRS - cria mapeamento para Commands e ajusta servico de Category

---
+++ Rental.Application/Mappings/DTOToCommandMappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Rental.Application.Categories.Commands;
+using Rental.Application.DTOs;
+using Rental.Application.Products.Commands;
+
+namespace Rental.Application.Mappings
+{
+    public class DTOToCommandMappingProfile : Profile
+    {
+        public DTOToCommandMappingProfile()
+        {
+            CreateMap<CategoryDTO, CategoryCreateCommand>();
+            CreateMap<CategoryDTO, CategoryUpdateCommand>();
+           
+            CreateMap<ProductDTO, ProductCreateCommand>();
+            CreateMap<ProductDTO, ProductUpdateCommand>();
+         
+        }
+    }
+}
--- Rental.Application/Services/CategoryService.cs
+++ Rental.Application/Services/CategoryService.cs
@@ -1,63 +1,64 @@
 ﻿using AutoMapper;
+using MediatR;
+using Rental.Application.Categories.Commands;
+using Rental.Application.Categories.Queries;
 using Rental.Application.DTOs;
 using Rental.Application.Interfaces;
-using Rental.Domain.Entities;
-using Rental.Domain.Interfaces;
 
 namespace Rental.Application.Services
 {
     public class CategoryService : ICategoryService
     {
-        private readonly ICategoryRepository _categoryRepository;
+        private readonly IMediator _mediator;
         private readonly IMapper _mapper;
 
-        public CategoryService(ICategoryRepository categoryRepository, IMapper mapper)
+        public CategoryService(IMediator mediator, IMapper mapper)
         {
-            _categoryRepository = categoryRepository;
+            _mediator = mediator;
             _mapper = mapper;
         }
 
         public async Task<CategoryDTO> CreateCategoryAsync(CategoryDTO categoryDto)
         {
-            var category = _mapper.Map<Category>(categoryDto);
-            await _categoryRepository.AddCategoryAsync(category);
+            var categoryCreateCommand = _mapper.Map<CategoryCreateCommand>(categoryDto);
+            var category = await _mediator.Send(categoryCreateCommand);
             return _mapper.Map<CategoryDTO>(category);
         }
 
         public async Task<bool> DeleteCategoryAsync(Guid id)
         {
-            await _categoryRepository.DeleteCategoryAsync(id);
+            var categoryRemoveCommand = new CategoryRemoveCommand(id);
+            await _mediator.Send(categoryRemoveCommand);
             return true;
         }
 
         public async Task<IEnumerable<CategoryDTO>> GetAllCategoriesAsync()
         {
-            var categories = await _categoryRepository.GetAllCategoriesAsync();
+            var categoryGetAllQuery = new GetCategoriesQuery();
+            var categories = await _mediator.Send(categoryGetAllQuery);
             return _mapper.Map<IEnumerable<CategoryDTO>>(categories);
 
         }
 
         public async Task<IEnumerable<CategoryDTO>> GetCategoriesByNameAsync(string name)
         {
-            var categories = await _categoryRepository.GetCategoriesByNameAsync(name);
+            var categoryGetByNameQuery = new GetCategoryByNameQuery(name);
+            var categories = await _mediator.Send(categoryGetByNameQuery);
             return _mapper.Map<IEnumerable<CategoryDTO>>(categories);
         }
 
         public async Task<CategoryDTO> GetCategoryByIdAsync(Guid id)
         {
-           var category = await _categoryRepository.GetCategoryByIdAsync(id);
-            if(category == null)
-            {
-                throw new KeyNotFoundException($"Category with id: {id} not found");
-            }
+            var categoryGetByIdQuery = new GetCategoryByIdQuery(id);
+            var category = await _mediator.Send(categoryGetByIdQuery);
             return _mapper.Map<CategoryDTO>(category);
         }
 
         public async Task<CategoryDTO> UpdateCategoryAsync(CategoryDTO categoryDto)
         {
-            var category = _mapper.Map<Category>(categoryDto);
-            await _categoryRepository.UpdateCategoryAsync(category);
-            return _mapper.Map<CategoryDTO>(category);
+            var categoryUpdateCommand = _mapper.Map<CategoryUpdateCommand>(categoryDto);
+            var updatedCategory = await _mediator.Send(categoryUpdateCommand);
+            return _mapper.Map<CategoryDTO>(updatedCategory);
         }
     }
 }
