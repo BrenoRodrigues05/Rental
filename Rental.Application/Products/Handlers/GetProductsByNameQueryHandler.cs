59a40136fc62a6cfa761da1c17d4fe9ed96ad21b
Breno Rodrigues Dos Santos <brenobr10@hotmail.com>
2026-09-20 13:08:16 -0300

    CQRS - cria query e handler para buscar products pelo nome

---
+++ Rental.Application/Products/Handlers/GetProductsByNameQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Rental.Application.Products.Queries;
+using Rental.Domain.Entities;
+using Rental.Domain.Interfaces;
+
+namespace Rental.Application.Products.Handlers
+{
+    public class GetProductsByNameQueryHandler : IRequestHandler<GetProductsByNameQuery, 
+        IEnumerable<Product>>
+    {
+        private readonly IProductRepository _productRepository;
+
+        public GetProductsByNameQueryHandler(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public async Task<IEnumerable<Product>> Handle(GetProductsByNameQuery request, 
+            CancellationToken cancellationToken)
+        {
+            var products = await _productRepository.GetProductsByNameAsync(request.Name);
+
+            return products;
+        }
+    }
+}
---
+++ Rental.Application/Products/Queries/GetProductsByNameQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using Rental.Domain.Entities;
+
+namespace Rental.Application.Products.Queries
+{
+    public class GetProductsByNameQuery : IRequest<IEnumerable<Product>>
+    {
+        public string Name { get; set; }
+        public GetProductsByNameQuery(string name)
+        {
+            Name = name;
+        }
+    }
+}
